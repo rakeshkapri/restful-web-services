ce04ce6360e44939269a7a859c9ad30bc95227ee
rakesh kapri <rakeshkapri@gmail.com>
2018-12-17 18:00:53 +0000

    removed unused imports

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/config/WebConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/config/WebConfig.java
@@ -1,12 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 

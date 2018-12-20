2540e96d3a9ba9c60661e238c3e8789ba25e8b7a
rakesh kapri <rakeshkapri@gmail.com>
2018-12-20 11:11:15 +0000

    Header based versioning

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/PersonVersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/PersonVersioningController.java
@@ -19,4 +19,15 @@ public class PersonVersioningController {
         Name name = new Name("Rakesh", "Kapri V2");
         return new PersonV2(name);
     }
+
+    @GetMapping(value = "/person", headers = "X-API-VERSION=1")
+    public PersonV1 getHeaderV1(){
+        return new PersonV1("Rakesh Kapri H1");
+    }
+
+    @GetMapping(value = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getHeaderV2(){
+        Name name = new Name("Rakesh", "Kapri H2");
+        return new PersonV2(name);
+    }
 }
